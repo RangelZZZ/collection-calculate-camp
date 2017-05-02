edf979925b90b9b15322a1bf6fb463ae6767a9dc
rangel <18792561236@163.com>
2017-05-02 17:03:13 +0800

    finished get_integer_interval_2.js

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,29 @@
 'use strict';
 
-function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+function get_integer_interval(number_a, number_b) {
+  const resultArray = [];
+  if (number_a - number_b > 0) {
+    for (var i = number_a; i >= number_b; i--) {
+      if (i % 2 === 0) {
+        resultArray.push(i);
+      }
+    }
+  }
+
+  if (number_b - number_a > 0) {
+    for (var  j= number_a; j <= number_b; j++) {
+      if (j % 2 === 0) {
+        resultArray.push(j);
+      }
+    }
+  }
+
+  if (number_a == number_b && number_a % 2 === 0) {
+    resultArray.push(number_a)
+  }
+
+  return resultArray;
 }
 
-module.exports = get_integer_interval_2;
+module.exports = get_integer_interval;
+
