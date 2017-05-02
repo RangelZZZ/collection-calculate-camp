d165ceb760de1eff0acd2567a6ec872a461f39f0
rangel <18792561236@163.com>
2017-05-02 18:40:48 +0800

    finished get_letter_interval

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,7 +1,21 @@
 'use strict';
 
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+  const resultArray = [];
+
+  if (number_a - number_b > 0) {
+    for (var i = number_a; i >= number_b; i--) {
+      resultArray.push(String.fromCharCode(i + 96));
+    }
+  }
+
+  if (number_a - number_b <= 0) {
+    for (var j = number_a; j <= number_b; j++) {
+      resultArray.push(String.fromCharCode(j + 96));
+    }
+  }
+
+  return resultArray;
 }
 
 module.exports = get_letter_interval;
