62565fdff8c088b7ba14ea79ee9f72cc686532e4
rangel <18792561236@163.com>
2017-05-02 18:05:58 +0800

    finished get_interspection_spec.js

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  //在这里写入代码
+  const resultArray = [];
+
+  collection_b.forEach(item => {
+    if (collection_a.find(element => element === item)) {
+      resultArray.push(item);
+    }
+  });
+  return resultArray;
+
 }
 
 module.exports = get_intersection;
