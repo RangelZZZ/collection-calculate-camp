2134a33827f215ffc32406de4c542d9560ba88c4
rangel <18792561236@163.com>
2017-05-06 10:01:14 +0800

    finished choose_no_repeat_number

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,8 +1,16 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
+  const resultArray = [];
 
-  //在这里写入代码
+  collection.forEach(item => {
+
+    if (!resultArray.find(element => element === item)) {
+      resultArray.push(item);
+    }
+  });
+
+  return resultArray;
 }
 
 module.exports = choose_no_repeat_number;
