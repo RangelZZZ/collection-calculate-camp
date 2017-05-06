dcc14b024cf28b5339c1d5195fd8e643aea744af
rangel <18792561236@163.com>
2017-05-06 22:23:06 +0800

    finished practice_3

--- practices/superposition_operation/collection_operation/practice_3.js
+++ practices/superposition_operation/collection_operation/practice_3.js
@@ -2,7 +2,7 @@
 
 function hybrid_operation_to_uneven(collection) {
 
-  //在这里写入代码
+  return collection.filter(item => item % 2 !== 0).map(item => item * 3 + 5).reduce((a, b)=> a + b);
 }
 
 module.exports = hybrid_operation_to_uneven;
