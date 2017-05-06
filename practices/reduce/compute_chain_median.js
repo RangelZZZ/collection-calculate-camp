7a83280cfa2fd0918bcbfb2b6b45fcc640fdb3f9
rangel <18792561236@163.com>
2017-05-06 18:28:33 +0800

    finish compute_chain_medain

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  //在这里写入代码
+  const resultArray = collection.split('->');
+
+  for (var i in resultArray) {
+    resultArray[i] = parseInt(resultArray[i]);
+  }
+  resultArray.sort((a, b) => a - b);
+
+  return (resultArray[resultArray.length /2] + resultArray[resultArray.length / 2 - 1]) / 2;
 }
 
 module.exports = compute_chain_median;
