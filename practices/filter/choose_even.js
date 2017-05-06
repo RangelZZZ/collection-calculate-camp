acb610878704968ebeceb35c4a5deb79fe21edc0
rangel <18792561236@163.com>
2017-05-06 09:36:57 +0800

    finished chooose_even

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -2,7 +2,7 @@
 
 function choose_even(collection) {
 
-  //在这里写入代码
+  return collection.filter(item => item % 2 === 0);
 }
 
 module.exports = choose_even;
--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,16 @@
 'use strict';
 
 function grouping_count(collection) {
+  const newCollection = collection.sort();
+  const result = {};
 
-  //在这里写入代码
+  for (var i in newCollection) {
+    if (newCollection[i] !== newCollection[i + 1]) {
+      result[newCollection[i]] = (newCollection.filter(item => item === newCollection[i])).length
+    }
+  }
+
+  return result;
 }
 
 module.exports = grouping_count;
