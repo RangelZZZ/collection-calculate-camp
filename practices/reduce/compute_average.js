3a68c0ec2492db231f04a522d27040baa3ee7ac7
rangel <18792561236@163.com>
2017-05-06 12:08:29 +0800

    refactor

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function compute_average(collection) {
-  return (collection.reduce((a, b)=> a + b)) / collection.length;
 
+  return (collection.reduce((a, b)=> a + b)) / collection.length;
 }
 
 module.exports = compute_average;
