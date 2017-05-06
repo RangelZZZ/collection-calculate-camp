e457b2764e4bf01ed49f8a9b4aabb83480c2cf7f
rangel <18792561236@163.com>
2017-05-06 11:56:28 +0800

    finish compare_collections

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function collect_max_number(collection) {
-
+  return collection.reduce((a, b)=> Math.max(a, b));
 }
 
 module.exports = collect_max_number;
--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  //在这里写入代码
+  collection_a.forEach(item => {
+    const location = collection_a.indexOf(item);
+    if (!collection_b.find(element => element === item && collection_b.indexOf(element) === location)) {
+      return false;
+    }
+  });
+
+  return true;
 }
 
 module.exports = compare_collections;
