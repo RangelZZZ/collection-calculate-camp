34e97e5aa7a7376327ada36f73678a170a1da942
rangel <18792561236@163.com>
2017-05-06 18:46:10 +0800

    finished compute_median

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -1,9 +1,15 @@
 'use strict';
 
 function compute_median(collection) {
-  //在这里写入代码
+
+  const resultArray = collection.sort((a, b)=> a - b);
+
+  return (resultArray.length % 2 === 0 ) ? (resultArray[resultArray.length / 2] + resultArray[resultArray.length / 2 - 1]) / 2 :
+      (resultArray[Math.floor(resultArray.length / 2)]);
 }
 
 module.exports = compute_median;
+
+
 
 
