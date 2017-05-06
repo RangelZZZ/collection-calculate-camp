c91f74192c5344400996889a5e8e98d800b4e5bc
rangel <18792561236@163.com>
2017-05-06 22:30:15 +0800

    finished average_uneen_spec.js

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function average_uneven(collection) {
+  const resultArray = collection.filter(item => item % 2 !== 0);
 
-  //在这里写入代码
+  return resultArray.reduce((a, b)=> a + b) / resultArray.length;
 }
 
 module.exports = average_uneven;
