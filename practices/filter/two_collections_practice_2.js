05be989157347f98831f24f5eb454193a665c36c
rangel <18792561236@163.com>
2017-05-06 10:06:49 +0800

    finish two_collections_practice_2

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -2,7 +2,7 @@
 
 function choose_no_common_elements(collection_a, collection_b) {
 
-  //在这里写入代码
+  return collection_a.filter(item => !collection_b.find(element => element === item));
 }
 
 module.exports = choose_no_common_elements;
