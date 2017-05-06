9f09c1c6c6c43424829a5580b16f46fd818ad7cb
rangel <18792561236@163.com>
2017-05-06 20:19:52 +0800

    finish letter_mapping_1_spec

--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function even_to_letter(collection) {
-
-  //在这里写入代码
+  return collection.filter(item => item % 2 === 0).map(item => String.fromCharCode(item + 96));
 }
 
 module.exports = even_to_letter;
