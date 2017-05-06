774b994a84e6ff3392b7945f2b071f4e505f0605
rangel <18792561236@163.com>
2017-05-06 20:05:45 +0800

    finish split_to_zero

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -1,7 +1,21 @@
 'use strict';
 
 function spilt_to_zero(number, interval) {
-  //在这里写入代码
+
+  let resultArray = [];
+
+  for (var i = number; i >= 0; i = i - interval) {
+
+    resultArray.push(parseFloat(i.toFixed(1)));
+  }
+
+  const element = resultArray[resultArray.length - 1];
+
+  if (element !== 0) {
+    resultArray.push(parseFloat((element - interval).toFixed(1)));
+  }
+
+  return resultArray;
 }
 
 module.exports = spilt_to_zero;
