58524c2d8269752ffd0524deb3fbbda4d2891423
rangel <18792561236@163.com>
2017-05-06 11:00:16 +0800

    finished number_map_to_word_over_26

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,13 @@
 'use strict';
-var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+var number_map_to_word_over_26 = function (collection) {
+
+  return collection.map(item => {
+    if (item <= 26) {
+      return String.fromCharCode(item + 96);
+    } else {
+      return `${String.fromCharCode(item / 26 + 96)}${String.fromCharCode(item % 26 + 96)}`;
+    }
+  })
 };
 
 module.exports = number_map_to_word_over_26;
