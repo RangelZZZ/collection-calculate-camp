77d6923866c71ed95501ef3f05a78d5ccffa7bb7
rangel <18792561236@163.com>
2017-05-06 10:04:26 +0800

    finish two_collections_practice_1

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -2,7 +2,8 @@
 
 function choose_common_elements(collection_a, collection_b) {
 
-  //在这里写入代码
+   return collection_a.filter(item => collection_b.find(element => element === item));
+
 }
 
 module.exports = choose_common_elements;
