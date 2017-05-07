8c9fdbfb9951baa57064ea4b192348e676ee015d
rangel <18792561236@163.com>
2017-05-07 09:19:37 +0800

    finish median_to_letter

--- practices/superposition_operation/interval_operation/median_to_letter.js
+++ practices/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function median_to_letter(collection) {
+  const result = Math.ceil(collection[collection.length / 2]);
 
-  //在这里写入代码
+  return `${String.fromCharCode(result / 26 + 96)}${String.fromCharCode(result % 26 + 96)}`;
 }
 
 module.exports = median_to_letter;
