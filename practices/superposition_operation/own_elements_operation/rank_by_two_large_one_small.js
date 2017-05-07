95cf401351a73f5a108808c1e70bff142f824860
rangel <18792561236@163.com>
2017-05-07 10:40:00 +0800

    finish rank_by_two_large_one_small

--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,16 @@
 'use strict';
-function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+function rank_by_two_large_one_small(collection) {
+  const newArray = collection.sort((a, b)=> a - b);
+
+  for (var i = 0; i < newArray.length - 3; i = i + 3) {
+    let item = newArray[i + 1];
+    let element = newArray[i + 2];
+
+    newArray[i + 2] = newArray[i];
+    newArray[i + 1] = element;
+    newArray[i] = item;
+  }
+
+  return newArray;
 }
 module.exports = rank_by_two_large_one_small;
