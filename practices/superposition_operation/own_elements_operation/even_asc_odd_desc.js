1504c24c23a282500ab1f9278f2db9195cc041e3
rangel <18792561236@163.com>
2017-05-07 09:38:14 +0800

    even_asc_odd_desc

--- practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,9 @@
 'use strict';
-var even_asc_odd_desc = function(collection){
+var even_asc_odd_desc = function (collection) {
+  const oddArray = collection.filter(item => item % 2 !== 0).sort((a, b)=> b - a);
+  const evenArray = collection.filter(item => item % 2 === 0).sort((a, b)=> a - b);
+
+  return evenArray.concat(oddArray);
 
 };
 module.exports = even_asc_odd_desc;
